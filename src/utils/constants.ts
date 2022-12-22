8388bc9baa5b64faced44a56e5612f4e365cf8f3
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2022-12-22 13:56:51 +0700

    delete firebase

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,13 +1,13 @@
 import { getLocalStorage } from "./helper/LocalStorage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCSfZkmjN8KMd9eJjJrfk2fqO-tP1ANUp4",
-  authDomain: "asc-real-estate.firebaseapp.com",
-  projectId: "asc-real-estate",
-  storageBucket: "asc-real-estate.appspot.com",
-  messagingSenderId: "825715756088",
-  appId: "1:825715756088:web:e609e32767d2ea2194aacd",
-  measurementId: "G-58QNS2TDG8"
+  apiKey: "XXXX",
+  authDomain: "XXXX",
+  projectId: "XXXX",
+  storageBucket: "XXXX",
+  messagingSenderId: "XXXX",
+  appId: "XXXX",
+  measurementId: "XXXX"
 };
 
 const LOCAL_STORAGE_LOGIN = "realEstate/token";
